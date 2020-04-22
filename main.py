6d0aab9ce5094722179e865d3d00c576eb459a79
Ilya Dolmatov <ilya.d.as@agiliumlabs.com>
2020-04-22 16:03:54 +0300

    вивести кількість витрачених ітерацій через pop-up

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.config import Config
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
@@ -14,16 +16,16 @@ def ferma_factorization(n, max_iteration=100):
     while n % a != 0:
         a += 1
     if a == n:
-        return 1, n, 'Prime number'
+        return 1, n, 'Prime number', 0
 
     if n <= 1:
-        return None, None, 'Error: Must be > 0'
+        return None, None, 'Error: Must be > 0', 0
 
     if n % 2 == 0:
-        return None, None, 'Error: must be odd'
+        return None, None, 'Error: must be odd', 0
 
     if is_square(n):
-        return int(n ** 0.5), int(n ** 0.5), 'Successful'
+        return int(n ** 0.5), int(n ** 0.5), 'Successful', 0
 
     x = int(n ** 0.5) + 1
     c = 0
@@ -31,20 +33,23 @@ def ferma_factorization(n, max_iteration=100):
         x += 1
         c += 1
         if c > max_iteration:
-            return None, None, 'Error: cannot calculate'
+            return None, None, 'Error: cannot calculate', max_iteration
 
     y = int((x * x - n) ** 0.5)
     a, b = x - y, x + y
 
-    return a, b, 'Successful'
+    return a, b, 'Successful', c
 
 
 class Container(GridLayout):
     def calculation(self):
         try:
             inp_number = int(self.number_input.text)
-            a, b, message = ferma_factorization(inp_number, int(self.iteration_input.text))
+            a, b, message, iter_n = ferma_factorization(inp_number, int(self.iteration_input.text))
             self.first_number.text, self.second_number.text, self.state_factorization.text = str(a), str(b), message
+            Popup(title="Iterations count", title_align="center",
+                  content=Label(text="{:d} iterations".format(iter_n)),
+                  size_hint=(.5, .5)).open()
         except:
             self.state_factorization.text = 'Invalid value'
 
